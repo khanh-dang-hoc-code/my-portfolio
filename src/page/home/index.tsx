4a30b2e54e7ef77c4ffb15176e65ddda2ae07b2b
Ngoc Khanh <khanhtn@ltsgroup.tech>
2022-11-07 15:28:05 +0700

    feature

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
 import Footer from '../../components/footer';
+import Header from '../../components/header';
 import Section from '../../components/section';
 
-
 const Home = () => {
+  const divRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  const [verticalOffset, setVerticalOffset] = useState(0);
+
+  useEffect(() => {
+   const destroy =  window.addEventListener('scroll', function () {
+      const verticalOffsetValue =
+        window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      setVerticalOffset(verticalOffsetValue);
+    });
+    return () => destroy;
+  }, [divRef]);
+
   return (
-    <div>
+    <div ref={divRef}>
+      <Header parentPosition={verticalOffset} />
+
+      <Section title="About me">
+        <div>section</div>
+      </Section>
+
+      <Section title="About me">
+        <div>section</div>
+      </Section>
+
+      <Section title="About me">
+        <div>section</div>
+      </Section>
+
+      <Section title="About me">
+        <div>section</div>
+      </Section>
+
+      <Section title="About me">
+        <div>section</div>
+      </Section>
+
       <Section title="About me">
         <div>section</div>
       </Section>
